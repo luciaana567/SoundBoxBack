11018816daf20335d957ea74677b597740b12a07
Luciana Araujo <luciaana.sn573@gmail.com>
2023-10-22 11:05:53 -0300

    creating and configuring project

---
+++ src/main/java/com/example/SoundBox/SoundBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.SoundBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundBoxApplication.class, args);
+	}
+
+}
